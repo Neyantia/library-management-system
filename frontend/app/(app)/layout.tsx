1456a4c140da14693646253a9dadc52808e62a28
JuliaOso <163736374+JuliaOso@users.noreply.github.com>
2026-06-03 18:29:16 +0200

    app

--- frontend/app/(app)/layout.tsx
+++ frontend/app/(app)/layout.tsx
@@ -8,14 +8,14 @@ export default function AppLayout({
   children: React.ReactNode;
 }) {
   return (
-    <>
+    <div className="page-wrapper">
       <NavBar />
       
-      <div className="app-layout">
-        <main>{children}</main>
-      </div>
+      <main className="app-content">
+        {children}
+      </main>
 
       <Footer />
-    </>
+    </div>
   );
 }
