4b904484151cce0abe1ea87c2629c3f8845a377a
JuliaOso <163736374+JuliaOso@users.noreply.github.com>
2026-06-06 08:42:49 +0200

    Delete frontend directory

--- frontend/app/admin/tables/create/categories/page.tsx
+++
@@ -1,5 +0,0 @@
-import CreateAdminForm from "@/components/CreateAdminForm";
-
-export default function CreateCategoryPage() {
-    return <CreateAdminForm type="category" />;
-}
--- frontend/components/LoadingScreen.tsx
+++
@@ -1,9 +0,0 @@
-import "@/styles/loading_page.css";
-
-export default function LoadingScreen() {
-    return (
-        <main className="loading-screen">
-            <div className="loading-moon img"></div>
-        </main>
-    );
-}
